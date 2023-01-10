b385e9f03ed42f4236477417ebf41624b0c67c4a
RoseVZ <priyarosev949@gmail.com>
2023-01-10 15:51:49 +0530

    sign in

--- src/Components/Pages/login.js
+++ src/Components/Pages/login.js
@@ -6,16 +6,32 @@ import TextField from '@mui/material/TextField';
 // import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import {useState} from 'react'
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { FormControlLabel,Checkbox,Link } from '@mui/material';
-
+import { login } from '../../actions/auth'
+import {connect} from 'react-redux'
 
 const theme = createTheme();
 
-export default function Login() {
+const Login = ({ login}) => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '' 
+});
+
+const { email, password } = formData;
+
+const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+const onSubmit = e => {
+    e.preventDefault();
+
+    login(email, password);
+};
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -43,7 +59,7 @@ export default function Login() {
           <Typography component="h1" variant="h5">
             Login
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box component="form" onSubmit={e=>onSubmit(e)} noValidate sx={{ mt: 1 }}>
             <TextField
               margin="normal"
               required
@@ -53,6 +69,10 @@ export default function Login() {
               name="email"
               autoComplete="email"
               autoFocus
+              onChange={e => onChange(e)}
+              value={email}
+              type='email'
+
             />
             <TextField
               margin="normal"
@@ -64,13 +84,15 @@ export default function Login() {
               id="password"
               minLength='6'
               autoComplete="current-password"
+              value={password}
+              onChange={e => onChange(e)}
             />
             <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
               label="Remember me"
             />
             <Button
-              type="submit"
+              type='submit'
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
@@ -90,3 +112,4 @@ export default function Login() {
     </ThemeProvider>
   );
 }
+export default connect(null, { login })(Login);
