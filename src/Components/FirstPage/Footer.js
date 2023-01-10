0e0b3bb549f57afcc7d7a9c9e1c18b9b70c52370
pratheethadiga <pratheethadiga1@gmail.com>
2023-01-10 08:36:48 +0530

    footer content

--- src/Components/FirstPage/Footer.js
+++ src/Components/FirstPage/Footer.js
@@ -5,22 +5,22 @@ export default function Footer(){
     return(
         <Grid container columnSpacing={0} rowSpacing={0} backgroundColor='black' color='white'>
             <Grid item xs={4} pl={1}>
-                <p><strong>Company</strong></p>
-                <p>Company</p>
-                <p>Company</p>
-                <p>Company</p>
+                <p><strong>Project by</strong></p>
+                <p>Pratheeth Adiga(20GACSE049)</p>
+                <p>Priyanka Rose Varghese(20GACSE050)</p>
+                {/* <p>Company</p> */}
             </Grid>
             <Grid item xs={4}>
+                <p>About Us</p>
                 <p>Contact Us</p>
-                <p>Contact Us</p>
-                <p>Contact Us</p>
-                <p>Contact Us</p>
+                <p>Legal</p>
+                <p>Our Locations</p>
             </Grid>
             <Grid item xs={4}>
-                <p>Legal</p>
-                <p>Legal</p>
-                <p>Legal</p>
-                <p>Legal</p>
+                <p>Under the Guidence of </p>
+                <p>Dr. Champa. H. N </p>
+                {/* <p>Legal</p> */}
+                {/* <p>Legal</p> */}
             </Grid>
         </Grid>
     )
