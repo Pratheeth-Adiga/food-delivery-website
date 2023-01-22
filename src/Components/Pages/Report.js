8269fab4b334ffb4de26868301bd0b54f5739d2d
pratheethadiga <pratheethadiga1@gmail.com>
2023-01-22 21:47:47 +0530

    react2print

--- src/Components/Pages/Report.js
+++ src/Components/Pages/Report.js
@@ -1,19 +1,17 @@
 import React, { useRef } from "react";
 import ReactToPrint from "react-to-print";
-import { Button } from "@mui/material";
+import { Button,Grid } from "@mui/material";
 
 export default function PrintComponent() {
   let componentRef = useRef();
 
   return (
       <div>
-                <ComponentToPrint ref={(el) => (componentRef = el)} />
+        <ComponentToPrint ref={(el) => (componentRef = el)} />
         <ReactToPrint
-          trigger={() => <Button color="success" variant="contained">Print</Button>}
+          trigger={() => <Grid container justifyContent='flex-end' pr={2}><Button color="success" variant="contained">Print</Button></Grid>}
           content={() => componentRef}
         />
-
-
       </div>
   );
 }
@@ -21,10 +19,10 @@ export default function PrintComponent() {
 class ComponentToPrint extends React.Component {
     render() {
       return (
-        <div>
+        <Grid pl={2}>
           <p>Project by Pratheeth and Priyanka </p>
           <p>Insert content here</p>
-        </div>
+        </Grid>
       );
     }
   }
