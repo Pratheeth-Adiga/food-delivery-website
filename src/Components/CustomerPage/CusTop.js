eaf3b35d1c566ff6ff0c65a79548fa11d7ab8715
RoseVZ <priyarosev949@gmail.com>
2023-01-26 17:27:55 +0530

    before it breaks

--- src/Components/CustomerPage/CusTop.js
+++ src/Components/CustomerPage/CusTop.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Avatar, Box,Grid,Link,Button } from "@mui/material";
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Avatar, Box, Grid, Link, Button } from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import stringAvatar from "../UserField/stringAvatar";
 import { logout } from "../../actions/auth";
 import { connect } from "react-redux";
@@ -8,65 +8,66 @@ import SearchBar from "../FirstPage/SearchBar";
 import store from "../../store";
 import { useNavigate } from "react-router-dom";
 
-const CusTop = ({ logout }) => {
-    const state = store.getState();
-    const User = state.auth.user
-    console.log(User)
-    const navigate = useNavigate()
-    const navigateto = () => {
-        
-        if (User === null)
-            navigate(`/cprofile/1`)
-        else
-            navigate(`/cprofile/${User.id}`)
-            
-    }
-    const gotoCart = () => {
-        if (User === null)
-            navigate(`/cart/1`)
-        else
-            navigate(`/cart/${User.id}`)
-    }
-    
-    return(
-        <Box>
-            <Grid container>
-                <Grid item xs={4}>
-                    <Link href="/album">
-                    <img src= "https://scalebranding.com/wp-content/uploads/2020/03/fun-food-logo.jpg" width='60px' height='60px' alt='logo'></img> 
-                    </Link>
-                </Grid>
-                <Grid item xs={5}>
-                <div class="search">
-                  <p>
-                    <SearchBar />
-                  </p>
-                </div>
-              </Grid>
-                <Grid item xs={1} pt={2} pl={10}>
-                    <Link onClick={ gotoCart} underline="none"><ShoppingCartIcon/></Link>   
-                </Grid>
-                <Grid item xs={1} pt={1} pl={1}>
-                    <Button
-                        variant="outlined"
-                        onClick={logout}
-                        href='/'
-                    >
-                        Logout
-                    </Button>
-                </Grid>
-                <Grid item xs={1} pt={0.5} >
-                    <Link underline="none" onClick={navigateto}>
-                        {User===null?<Avatar {...stringAvatar("Priyanka")}/>:<Avatar {...stringAvatar(User.email)}/>}
-                        
-                    </Link>
-                </Grid>
-            </Grid>
-        </Box>
-    )
-}
 
-const mapStateToProps = state => ({
-    isAuthenticated: state.auth.isAuthenticated
+const CusTop = ({ logout }) => {
+  const state = store.getState();
+  const User = state.auth.user;
+  console.log(User);
+  const navigate = useNavigate();
+  const navigateto = () => {
+    if (User === null) navigate(`/cprofile/1`);
+    else navigate(`/cprofile/${User.id}`);
+  };
+  const gotoCart = () => {
+    if (User === null) navigate(`/cart/1`);
+    else navigate(`/cart/${User.id}`);
+  };
+
+  return (
+    <Box>
+      <Grid container>
+        <Grid item xs={4}>
+          <Link href="/album">
+            <img
+              src="https://scalebranding.com/wp-content/uploads/2020/03/fun-food-logo.jpg"
+              width="60px"
+              height="60px"
+              alt="logo"
+            ></img>
+          </Link>
+        </Grid>
+        <Grid item xs={5}>
+          <div class="search">
+            <p>
+              <SearchBar />
+            </p>
+          </div>
+        </Grid>
+        <Grid item xs={1} pt={2} pl={10}>
+          <Link onClick={gotoCart} underline="none">
+            <ShoppingCartIcon />
+          </Link>
+        </Grid>
+        <Grid item xs={1} pt={1} pl={1}>
+          <Button variant="outlined" onClick={logout} href="/">
+            Logout
+          </Button>
+        </Grid>
+        <Grid item xs={1} pt={0.5}>
+          <Link underline="none" onClick={navigateto}>
+            {User === null ? (
+              <Avatar {...stringAvatar("Priyanka")} />
+            ) : (
+              <Avatar {...stringAvatar(User.email)} />
+            )}
+          </Link>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
 });
-export default connect(mapStateToProps,{logout})(CusTop);
+export default connect(mapStateToProps, { logout })(CusTop);
