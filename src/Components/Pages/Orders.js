797fe3e65c85a55433fb3f11aaafd4c0a351eec9
pratheethadiga <pratheethadiga1@gmail.com>
2023-02-24 21:46:18 +0530

    update

--- src/Components/Pages/Orders.js
+++ src/Components/Pages/Orders.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { Grid, Button, Typography, Card, CardContent } from "@mui/material";
+import { Grid, Button, Typography, Card, CardContent,Paper } from "@mui/material";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+
+let idli = "https://www.thespruceeats.com/thmb/6j6Ne_4F62_uigRCvTZYVykcHhc=/2122x1415/filters:fill(auto,1)/idli-56a510b63df78cf772862c34.jpg";
+let dosa = "https://i.ytimg.com/vi/CCab5oh0ZOc/maxresdefault.jpg";
+let roti = "https://3.bp.blogspot.com/-TsR1mrtpIic/WTgVxZcV4KI/AAAAAAAACWQ/g626zbTr0EwI_lGHlCRA9VO3SvV1ROe-gCLcB/s1600/loonapix_14968509663081167873.jpg";
+let uppit = "https://i.ytimg.com/vi/r_s15RTEZlk/maxresdefault.jpg";
+let noodles = "https://www.funfoodfrolic.com/wp-content/uploads/2020/09/Garlic-Noodles-Thumbnail.jpg";
+let samosa = "https://curlytales.com/wp-content/uploads/2019/11/Samosa-Recipe.jpg";
+let panipuri = "https://1.bp.blogspot.com/-MjhYOpRFJeM/UbchEdUhDVI/AAAAAAAACcE/HTdJ-EghQlc/s1600/pani+puri.JPG";
+let abc_list = [idli,dosa,roti,uppit,noodles,samosa,panipuri];
 
 export default function Orders() {
   const { id,oid } = useParams();
@@ -18,7 +27,7 @@ export default function Orders() {
     fetchData();
     console.log(typeof data);
   }, []);
-    const navigate =useNavigate()
+    const navigate = useNavigate()
    
   return (
     <div>
@@ -26,35 +35,47 @@ export default function Orders() {
         <h4>Nothing in Order</h4>
       ) : (
         <div>
-          <h4>The Order Items:</h4>
+          <Typography variant="h4" pt={3} pb={4} pl={2}>The Orders which are Pending:</Typography>
+          <Paper elevation={2}>
           <Card>
             {data.map((x) => (
-              <CardContent >
+              <CardContent>
                 <Grid container>
-                        <Grid item xs={1}>
-                            <h3>
-                                {x?.Name}
-                            </h3>
+                    <Grid item xs={2} pl={5}>
                     <img
-                      src="http://chinabuffetatlantic.com/custom/3.jpg"
+                      src={abc_list[2]}
                       alt="food"
-                      width="65px"
-                      height="65px"
-                    ></img>
-                  </Grid>
-                  <Grid item xs={10}>
-                            <Typography variant="h6"></Typography>
-                    
-                  </Grid>
-                  {/* <Grid item justifyContent="center" pt={2}>
-                    <Button variant="contained" color="success"  onClick={()=>checkItems(x?.Food_Id)}>
-                      Check Order
-                    </Button>
-                  </Grid> */}
+                      width="100px"
+                      height="100px"
+                    >
+                    </img>
+                    </Grid>
+                    <Grid item xs={3} pt={2}>
+                            <Typography variant="h5">
+                                {x?.Name}
+                            </Typography>
+                            <Typography variant="body">
+                                Priority or Quantity
+                            </Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                    </Grid>
+                    <Grid item xs={1} pt={4}>
+                      <Button variant="contained" color="success">
+                        Completed
+                      </Button>
+                    </Grid>
+                    <Grid item xs={2} pt={4} pl={3}>
+                      <Button variant="contained" color="error">
+                        Cancel Order
+                      </Button>
+                    </Grid>
+
                 </Grid>
               </CardContent>
             ))}
           </Card>
+          </Paper>
         </div>
       )}
     </div>
