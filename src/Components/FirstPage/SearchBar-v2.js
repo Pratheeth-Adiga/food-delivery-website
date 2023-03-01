559aafe5d3f033f76fcdb2e0b13707e097cbc552
pratheethadiga <pratheethadiga1@gmail.com>
2023-03-01 08:28:20 +0530

    added navigate

--- src/Components/FirstPage/SearchBar-v2.js
+++ src/Components/FirstPage/SearchBar-v2.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Autocomplete from '@mui/material/Autocomplete';
+import { useNavigate } from 'react-router-dom';
 
 export default function Bar() {
   return (
