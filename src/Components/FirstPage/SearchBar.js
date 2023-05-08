3c4351be3f879bb6b1c39f7f423a3a98d1854409
pratheethadiga <pratheethadiga1@gmail.com>
2023-05-08 20:28:53 +0530

    search

--- src/Components/FirstPage/SearchBar.js
+++ src/Components/FirstPage/SearchBar.js
@@ -1,18 +1,35 @@
-// import * as react from 'react';
-import { Grid,Button } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-import Bar from './SearchBar-v2';
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Autocomplete from '@mui/material/Autocomplete';
 
-export default function SearchBar(){
-    return(
-        <Grid container>
-        {/* <TextField id="outlined-search" label="Search field" type="search" size='small'/> */}
-        <Grid item xs={6}><Bar /></Grid>
-        <Grid item xs={1}><Button size="large" variant='outlined'>
-            <SearchIcon/>
-        </Button></Grid>
-
-        
-        </Grid>
-    )
+export default function SearchBar() {
+  return (
+    <Stack spacing={2} sx={{ width: 300 }}>
+      <Autocomplete
+        freeSolo
+        id="hotel search"
+        disableClearable
+        options={Hotels.map((option) => option.title)}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Search"
+            size='small'
+            InputProps={{
+              ...params.InputProps,
+              type: 'search',
+            }}
+          />
+        )}
+      />
+    </Stack>
+  );
 }
+
+const Hotels = [
+  { title: 'Adigas'},
+  { title: 'Chat Corner'},
+  { title: 'Pizza Corner'},
+  { title: 'Udupi Hotel'}
+];
